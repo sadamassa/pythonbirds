6da768ebb68756eda481274b4c56af05f79fc4be
Sadamassa <sadamassa@gmail.com>
2020-03-05 22:05:53 -0300

    criado atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=51):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Sada')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Sadamassa'
+    print(p.nome)
+    print(p.idade)
+
