b6a09a77f42f37b2746a6b4b4110cdfe8aba0efc
Sadamassa <sadamassa@gmail.com>
2020-03-08 22:37:12 -0300

    implementacao direcao

--- oo/carro.py
+++ oo/carro.py
@@ -107,5 +107,26 @@ class Motor:
         self.velocidade -=2
         self.velocidade = max(0, self.velocidade)
 
+NORTE='Norte'
+SUL='Sul'
+LESTE='Leste'
+OESTE='Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct={
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct={
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
 
